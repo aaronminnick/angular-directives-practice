29db253d7c8e17dfae03983ab24f5205bf8ad3e3
Aaron Minnick <abminnick@gmail.com>
2022-06-24 14:38:20 -0700

    add images array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'directives-practice';
+  images = [
+    {
+      title: 'at the beach',
+      url: 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhY2h8ZW58MHx8MHx8&auto=format&fit=crop&w=900&q=60'
+    },
+    {
+      title: 'at the park',
+      url: 'https://images.unsplash.com/photo-1519331379826-f10be5486c6f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGFya3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=900&q=60'
+    },
+    {
+      title: 'at the mall',
+      url: 'https://images.unsplash.com/photo-1522684462852-01b24e76b77d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bWFsbHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=900&q=60'
+    },
+    {
+      title: 'at the forest',
+      url: 'https://images.unsplash.com/photo-1542202229-7d93c33f5d07?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Zm9yZXN0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60'
+    },
+    {
+      title: 'at the river',
+      url: 'https://images.unsplash.com/photo-1473448912268-2022ce9509d8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Zm9yZXN0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60'
+    }
+  ]
 }
